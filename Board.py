2002d984b4cebf9583f20bea01edb2a569962795
Anas El Yousfi <anaselyousfi@gmail.com>
2021-07-07 15:24:43 -0400

    board quick fix

--- Board.py
+++ Board.py
@@ -2,18 +2,21 @@ from Ship import Ship
 
 class Board:
 # 10 x 10 board
-def build_game_board(board):
-    for item in range(10):
-        board.append(["O"] * 10)
+  board = []
 
-def show_board(board):
-    for row in board:
+  def build_game_board(board):
+    for item in range(10):
+        board.append(["O"] * 10),
+
+  def show_board(board):
+      for row in board:
+
         print(" ".join(row))
 
-# Defining ships locations
-def load_game(board):
+  def load_game(board):
     print("BATTLESHIP")
-    del board[:]
-    build_game_board(board)
-    show_board(board)
-ship_points = load_game(game_board)
+    print("\033[1m0 1 2 3 4 5 6 7 8 9 \033[1m")
+
+  load_game(board)
+  build_game_board(board)
+  show_board(board)
