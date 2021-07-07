76b551dc0c7fa127945341a77b111f25ea029d70
Trajon Waters <trajonwa@gmail.com>
2021-07-07 14:02:11 -0400

    Still editing play() function.

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import os
-import random
 import sys
+from time import sleep
 from player import Player
 from ship import Ship
 from board import Board
@@ -12,13 +12,32 @@ class Game():
         self.player2 = player2
         self.current_player = None
         self.next_player = None
+        self.round = 0
 
-    def choose_first_player(self):
-        self.current_player = random.choice([self.player1, self.player2])
+    def get_current_player(self):
+        players = [self.player1, self.player2]
+        return players[self.round % len(players)]
 
-        if self.current_player == self.player1:
-            self.next_player = self.player2
+    def get_next_player(self):
+        players = [self.__player_1, self.__player_2]
+        return players[(self.round + 1) % len(players)]
+
+    def clear_screen():
+
+        if os.name == 'posix':
+            _ = os.system('clear')
         else:
-            self.next_player = self.player1
+            _ = os.system('cls')
 
+    def play():
+        """
+        while True:
+            #Clear Screen
+            clear_screen()
+            sleep(1)
 
+            current_player = self.get_current_player()
+            current_player.get_board()
+
+            print(f"{current_player}'s turn!")
+        """
