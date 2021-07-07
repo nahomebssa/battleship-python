81928fdbdc38ae3953be8298dfbf3638574afd38
Benson <44655043+kamau96@users.noreply.github.com>
2021-07-07 10:48:22 -0400

    Added docstring

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ class Ship: pass
 class Board: pass
 
 class Player: 
+    '''
+    This class defines a player who will play the game against another player. This class is able to add ships to the board.
+    To ensure that ship position is hidden, we only store the ship coordinates and refer to them.
+    Furthermore, we can remove a specific ship if the player wants to.
+    This class will also enable us to know if a certain player has won the game.
+    '''
     def __init__(self, playerName, shipClass, boardClass, playerScore = 0) -> None:
         self.playerName = playerName
         self.playerScore = playerScore
@@ -21,7 +27,7 @@ class Player:
             if orientation == 'v':
                 print("Please note that ships will be built top -> down")
                 placement = tuple(input("Please specify starting coordinate i.e (0, 0): "))
-                for pos in range(ships.length):
+                for _ in range(ships.length):
                     if ships.name not in ship_location:
                         ship_location[ships.name] = [placement]
                     else:
@@ -30,7 +36,7 @@ class Player:
             elif orientation == 'h':
                 print("Please note that ships will be built left -> right")
                 placement = tuple(input("Please specify which row to place ship, i.e 0-9: "))
-                for pos in range(ships.length):
+                for _ in range(ships.length):
                     if ships.name not in ship_location:
                         ship_location[ships.name] = [placement]
                     else:
