9f9ad273d8f2b950b5b7323316b3e159f578f5af
Nahom Ebssa <enahom99@outlook.com>
2021-07-09 11:19:24 -0400

    Fixes and added getters to Ship class

--- Ship.py
+++ Ship.py
@@ -1,45 +1,53 @@
 class Ship:
+    """
+    Class to represent a ship on the board, represented as
+    a collection (set) of cells
+    """
 
     __name: str
-    __size: tuple[int, int]
+    __size: int
     __cells: set[tuple[int, int]]
     __cells_attacked: set[tuple[int, int]]
 
     __SHIP_TYPES = {
-        "Carrier": 5,
-        "Battleship": 4,
-        "Cruiser": 3,
-        "Submarine": 3,
-        "Destroyer": 2,
+        "carrier": 5,
+        "battleship": 4,
+        "cruiser": 3,
+        "submarine": 3,
+        "destroyer": 2,
     }
 
     def __init__(self, name: str):
         self.__name = name
-        self.__size = Ship.__SHIP_TYPES[self.__name]
+        self.__size = Ship.__SHIP_TYPES[self.__name.lower()]
         self.__cells = set()
         self.__cells_attacked = set()
 
-    
     def get_name(self) -> str:
         return self.__name
 
-    def get_size(self) -> tuple[int, int]:
+    def get_size(self) -> int:
         return self.__size
+
+    def get_cells(self) -> set[tuple[int, int]]:
+        return self.__cells
+
+    def get_cells_attacked(self) -> set[tuple[int, int]]:
+        return self.__cells_attacked
 
     def add_cell(self, cell: tuple[int, int]):
         self.__cells.add(cell)
 
-    
     def has_sunk(self) -> bool:
         """
         Determines whether this ship has been sunk
         """
         return len(self.__cells) == 0
-  
+
     def receive_attack(self, cell: tuple[int, int]) -> bool:
         """
         Removes the incoming cell from this ship's set of cells
-        @param cell The cell
+        @param cell The cell which was requested to receive the attack
         @returns True if the cell was attacked, false otherwise
         """
         if cell in self.__cells:
@@ -47,6 +55,15 @@ class Ship:
             self.__cells_attacked.add(cell)
             return True
         return False
-        
 
+    def __str__(self) -> str:
+        return "#"
 
+    def __repr__(self) -> str:
+        return f"Ship{{{self.__name}, {self.__size}}}"
+
+    def get_cells__for_testing(self) -> set[tuple[int, int]]:
+        return self.__cells
+
+    def get_cells_attacked__for_testing(self) -> set[tuple[int, int]]:
+        return self.__cells_attacked
