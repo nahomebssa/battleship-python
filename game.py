4a23081b39fe03d3a0c91ec1df01b8fbd806fb9d
Trajon Waters <trajonwa@gmail.com>
2021-07-09 07:14:53 -0400

    Added more to the play() function to make the game work.

--- game.py
+++ game.py
@@ -1,16 +1,15 @@
 import os
-import sys
 from time import sleep
 from player import Player
 from Ship import Ship
-# from board import Board
+from Board import Board
+
 
 class Game:
 
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-        self.current_player = None
         self.round = 0
 
     def get_current_player(self):
@@ -18,7 +17,7 @@ class Game:
         return players[self.round % len(players)]
 
     def get_next_player(self):
-        players = [self.player1, self.player1]
+        players = [self.player1, self.player2]
         return players[(self.round + 1) % len(players)]
 
     def clear_screen(self):
@@ -29,15 +28,47 @@ class Game:
             _ = os.system('cls')
 
     def play(self):
-        pass
-        '''
-        while True:
-            #Clear Screen
-            clear_screen()
-            sleep(1)
+        is_running = True
+        winner = None
 
-            current_player = self.get_current_player()
-            current_player.get_board()
+        while is_running:
+            '''
+            Clears the screen
+            '''
+            self.clear_screen()
+            sleep(0.1)
 
-            print(f"{current_player}'s turn!")
-        '''
+            '''
+            # Retrieves the current player's board and alerts them that it's their turn.
+            '''
+            self.get_next_player().print_board()
+            print(self.get_current_player().playerName + "'s turn!")
+
+            '''
+            # Current player selects where they want to attack, and it's stored in a variable.
+            '''
+            selected_coordinates = self.get_current_player().make_turn()
+
+            ''' 
+            If there is a hit on the next player's board, the current player is told so. 
+            If not, they're told it was a miss.
+            '''
+            if self.get_next_player().attacked(selected_coordinates):
+                print("It was a hit!")
+            else:
+                print("It's a miss!")
+
+            if self.get_current_player().any_remaining_ships():
+                winner = self.get_next_player()
+                is_running = False
+
+            if self.get_next_player().any_remaining_ships():
+                winner = self.get_current_player()
+                is_running = False
+            '''
+            Increment the round variable to be used in the get_current_player() and get_next_player() methods.
+            '''
+            self.round += 1
+
+        return winner
+
